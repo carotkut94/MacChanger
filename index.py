c878e23fc880bccecbee16793ad26be7a94783dc
Sidhant Rajora <carotkut12@gmail.com>
2017-03-15 23:06:44 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ if os.name == "posix":
         hexMac = ':'.join(str(macAddress)[i:i+2] for i in range(0, len(str(macAddress)), 2))
         print (hexMac)
         os.system("sudo -S ifconfig en0 ether "+hexMac)
-elif os.name == "nt":
+""" Work is in process for other OS elif os.name == "nt":
     macAddress = input("Enter mac to change to:")
     if len(str(macAddress)) == 12:
         hexMac = ':'.join(str(macAddress)[i:i + 2] for i in range(0, len(str(macAddress)), 2))
         print(hexMac)
-        os.system("" + hexMac)
+        os.system("" + hexMac)"""
